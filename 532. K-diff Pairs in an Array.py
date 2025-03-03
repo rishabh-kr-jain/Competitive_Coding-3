9112188911ab812ef2afbf6a1112623dcf6bffcd
Rishabh <24757929+rishabh-kr-jain@users.noreply.github.com>
2025-03-02 16:30:00 -0800

    Create 532. K-diff Pairs in an Array.py

---
+++ 532. K-diff Pairs in an Array.py
@@ -0,0 +1,19 @@
+class Solution:
+    def findPairs(self, nums: List[int], k: int) -> int:
+        hmap= dict()
+        dup=set()
+        for i in range(len(nums)):
+            if nums[i] in hmap:
+                dup.add(nums[i])
+            hmap[nums[i]]=i
+        hset=set()
+        if k==0:
+            return len(dup)
+        else:
+            for j in hmap:
+                if (j+k) in hmap:
+                    hset.add((j,j+k))
+        return len(hset)
+
+
+        
